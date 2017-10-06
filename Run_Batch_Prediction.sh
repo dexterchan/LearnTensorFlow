6c452799402223b9281fb929127ade22bfc3722b
dexterchan <dexterchan@gmail.com>
2017-10-06 16:35:31 +0800

    learncheckin

--- Run_Batch_Prediction.sh
+++ Run_Batch_Prediction.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-export JOB_NAME=sonar_prediction
+export BASE_JOB_NAME=sonar_prediction
+now=$(date +"%Y%m%d_%H%M%S")
+JOB_NAME="$BASE_JOB_NAME$now"
 
-export GCS_JOB_DIR=gs://dextest/mltest_census
-export BASE_JOB_NAME=census_20171005_163230
+
+
+export GCS_JOB_DIR=gs://dextest/mltest_sonar
+export BASE_JOB_NAME=mltest_sonar_20171005_193554
+
+
+export sampleJson=gs://dextest/mltest_sonar/sampleOutput.sampledata.R.json
 
 #--runtime-version 1.2 \
 gcloud ml-engine jobs submit prediction $JOB_NAME \
@@ -11,5 +18,5 @@ gcloud ml-engine jobs submit prediction $JOB_NAME \
     --version v1 \
     --data-format TEXT \
     --region us-central1 \
-    --input-paths gs://cloudml-public/testdata/prediction/census.json \
+    --input-paths $sampleJson \
     --output-path $GCS_JOB_DIR/predictions
